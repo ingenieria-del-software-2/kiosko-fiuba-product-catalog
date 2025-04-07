595ccb79a78768d1cd3939054439fe0d9336111e
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2025-04-07 12:06:04 -0300

    fix: merge all migration heads to resolve conflicts

--- src/migrations/versions/2025-04-07-13-16_e62713368bc7.py
+++ src/migrations/versions/2025-04-07-13-16_e62713368bc7.py
@@ -1,7 +1,7 @@
 """merge populate_slugs and e3bbdc30424c.
 
 Revision ID: e62713368bc7
-Revises: e3bbdc30424c, populate_slugs
+Revises: e3bbdc30424c, populate_slugs, modified_e3bbdc30424c, final_tables
 Create Date: 2025-04-07 13:16:37.476955
 
 """
@@ -10,14 +10,21 @@ from typing import Sequence, Union
 
 # revision identifiers, used by Alembic.
 revision: str = "e62713368bc7"
-down_revision: Union[str, None] = ("e3bbdc30424c", "populate_slugs")
+down_revision: Union[str, None] = (
+    "e3bbdc30424c",
+    "populate_slugs",
+    "modified_e3bbdc30424c",
+    "final_tables",
+)
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
     """Run the migration."""
+    pass
 
 
 def downgrade() -> None:
     """Undo the migration."""
+    pass
